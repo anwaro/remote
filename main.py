0106fa6f13ad524cb341eec6b9d9a506eaf9b524
Łukasz Micał <lukasz.mical@sarigato.com>
2017-10-23 21:40:41 +0200

    remove unnecessary code

--- main.py
+++ main.py
@@ -48,9 +48,3 @@ class RemoteApp:
 if __name__ == "__main__":
     app = RemoteApp()
     app.run()
-    import sys, os
-
-    print('sys.argv[0] =', sys.argv[0])
-    pathname = os.path.dirname(sys.argv[0])
-    print('path =', pathname)
-    print('full path =', os.path.abspath(pathname)) 
